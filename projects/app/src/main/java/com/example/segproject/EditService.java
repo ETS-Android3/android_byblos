4a04aa437171db841f9406b71bca0ace474c9b86
Caitlin Lim <caitlim777@gmail.com>
2021-10-31 17:31:24 -0400

    services

--- projects/app/src/main/java/com/example/segproject/EditService.java
+++ projects/app/src/main/java/com/example/segproject/EditService.java
@@ -2,14 +2,34 @@ package com.example.segproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EditService extends AppCompatActivity {
     // here the admin will be able to add edit and delete services
+
+    private Button addservice;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_service);
+
+        addservice = (Button) findViewById(R.id.addServiceBTN);
+        addservice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAddGlobalService();
+            }
+        });
+    }
+
+    public void openAddGlobalService(){
+        Intent intent = new Intent(this,AddGlobalService.class);
+        startActivity(intent);
+
     }
 
 
