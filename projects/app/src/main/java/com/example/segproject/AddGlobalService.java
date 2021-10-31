d325c552e42df4d6ccd126d2e329845ca5de6e60
Caitlin Lim <caitlim777@gmail.com>
2021-10-31 18:53:04 -0400

    caitlin commit

--- projects/app/src/main/java/com/example/segproject/AddGlobalService.java
+++ projects/app/src/main/java/com/example/segproject/AddGlobalService.java
@@ -3,6 +3,10 @@ package com.example.segproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -14,15 +18,40 @@ public class AddGlobalService extends AppCompatActivity {
     // here the admin will be able to add new services to the database
 
     DatabaseReference dbServices;
+    EditText etname;
+    EditText etrate;
+    Button btcreate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_global_service);
 
-        DatabaseReference dbServices = FirebaseDatabase.getInstance().getReference("services");
+        etname = findViewById(R.id.newServiceName);
+        etrate = findViewById(R.id.newServiceHourlyRate);
+        btcreate = findViewById(R.id.submitNewService);
+        dbServices = FirebaseDatabase.getInstance().getReference().child("Service");
+
+        btcreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createNewService();
+            }
+        });
+
+
     }
 
+    private void createNewService(){
+        String name = etname.getText().toString();
+        String rate = etrate.getText().toString();
+        NewService ns = new NewService(name,rate);
+
+        dbServices.push().setValue(ns);
+        Toast.makeText(this,"New service created", Toast.LENGTH_SHORT).show();
+
+    }
 
 
 }
---
+++ projects/app/src/main/java/com/example/segproject/NewService.java
@@ -0,0 +1,22 @@
+package com.example.segproject;
+
+public class NewService {
+
+    String name;
+    String rate;
+
+    public NewService(String name, String rate) {
+        this.name = name;
+        this.rate = rate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRate() {
+        return rate;
+    }
+
+
+}
