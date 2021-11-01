358a8a82fc6d23a6b427da73be973427dde839b4
e1ayna <etian031@uottawa.ca>
2021-11-01 01:26:58 -0400

    new

--- projects/app/src/main/java/com/example/segproject/EditService.java
+++ projects/app/src/main/java/com/example/segproject/EditService.java
@@ -2,9 +2,13 @@ package com.example.segproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.os.Bundle;
+import android.annotation.*;
+import android.content.Intent;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -13,7 +17,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class EditService extends AppCompatActivity {
     // here the admin will be able to add edit and delete services
@@ -36,24 +39,29 @@ public class EditService extends AppCompatActivity {
         dbRef.addChildEventListener(new ChildEventListener() {
 
             @Override
-            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                String value = DataSnapshot.getValue(Service.class).toString();
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String value = dataSnapshot.getValue(Service.class).toString();
                 arrayList.add(value);
                 arrayAdapter.notifyDataSetChanged();
             }
 
             @Override
-            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
 
             }
 
             @Override
-            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
 
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
 
             }
         });
@@ -74,15 +82,4 @@ public class EditService extends AppCompatActivity {
     }
 
 
-
-    private void createTruckRental(){
-
-// send service to db
-    }
-    private void createCarRental(){
-// send service to db
-    }
-    private void createMovingAssistance(){
-//send service to db
-    }
 }
