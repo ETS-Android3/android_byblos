74760e9b1ce1b8e4bc47b1b412b37a6f49563201
e1ayna <etian031@uottawa.ca>
2021-11-20 15:03:12 -0500

    edit services

--- projects/app/src/main/java/com/example/segproject/EditService.java
+++ projects/app/src/main/java/com/example/segproject/EditService.java
@@ -15,6 +15,7 @@ import android.widget.ListView;
 import android.os.Bundle;
 import android.content.Intent;
 import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -42,6 +43,7 @@ public class EditService extends AppCompatActivity {
             cbmovingendlocation, cbarea, cbkmdriven, cbnumberofmovers, cbnumberofboxes, cbisoffered;
 
     String newName;
+    double newRate;
     double rate;
     boolean isOffered;
     boolean firstName;
@@ -87,26 +89,35 @@ public class EditService extends AppCompatActivity {
 
         //listen for service long press.
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 NewService serv = arrayList.get(position);
 
-                deleteServiceDialog(serv.getName(),serv.getServiceID());
+                deleteServiceDialog(serv.getName(),serv.getServiceID(),serv.getRate());
                 return false;
             }
         });
 
     }
 
-    private void deleteServiceDialog(String name,  String servID){
+    private void deleteServiceDialog(String name,  String servID, double rate){
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
         LayoutInflater inflater = getLayoutInflater();
         final View dialogView = inflater.inflate(R.layout.update_delete_service, null);
         dialogBuilder.setView(dialogView);
 
-        editTextName = (EditText) dialogView.findViewById(R.id.nameEditText);
-        editTextRate  = (EditText) dialogView.findViewById(R.id.rateEditText);
+        // convert rate to string
+        String rateText = String.valueOf(rate);
 
+        // service name and service rate are shown and can be edited
+        editTextName = (EditText) dialogView.findViewById(R.id.nameEditText);
+        editTextName.setText(name, TextView.BufferType.EDITABLE);
+        editTextRate  = (EditText) dialogView.findViewById(R.id.rateEditText);
+        editTextRate.setText(rateText, TextView.BufferType.EDITABLE);
+
+        // appropriate checkboxes should be checked
+        // loop through the service's attributes and set appropriate checkboxes to checked
 
         cbisoffered = dialogView.findViewById(R.id.isOfferedUpdate);
         cbfirstName = dialogView.findViewById(R.id.firstName);
@@ -132,6 +143,90 @@ public class EditService extends AppCompatActivity {
         cbnumberofboxes = dialogView.findViewById(R.id.numberofboxes);
 
 
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                cbisoffered.setChecked(false);
+                cbfirstName.setChecked(false);
+                cblastName.setChecked(false);
+                cbdob.setChecked(false);
+                cbaddress.setChecked(false);
+                cbemail.setChecked(false);
+                cbG1.setChecked(false);
+                cbG2.setChecked(false);
+                cbG3.setChecked(false);
+                cbcompact.setChecked(false);
+                cbintermediate.setChecked(false);
+                cbSUV.setChecked(false);
+                cbpickupdate.setChecked(false);
+                cbpickuptime.setChecked(false);
+                cbreturndate.setChecked(false);
+                cbreturntime.setChecked(false);
+                cbmovingstartlocation.setChecked(false);
+                cbmovingendlocation.setChecked(false);
+                cbarea.setChecked(false);
+                cbkmdriven.setChecked(false);
+                cbnumberofmovers.setChecked(false);
+                cbnumberofboxes.setChecked(false);
+
+                for(DataSnapshot info : snapshot.getChildren()){
+                    NewService serv = info.getValue(NewService.class);
+                    if (serv.isOffered()){
+                        cbisoffered.setChecked(true);
+                    }if (serv.isFirstName()){
+                        cbfirstName.setChecked(true);
+                    }if (serv.isLastName()){
+                        cblastName.setChecked(true);
+                    }if (serv.isDob()){
+                        cbdob.setChecked(true);
+                    }if (serv.isAddress()){
+                        cbaddress.setChecked(true);
+                    }if (serv.isEmail()){
+                        cbemail.setChecked(true);
+                    }if (serv.isG1()){
+                        cbG1.setChecked(true);
+                    }if (serv.isG2()){
+                        cbG2.setChecked(true);
+                    }if (serv.isG3()){
+                        cbG3.setChecked(true);
+                    }if (serv.isCompact()){
+                        cbcompact.setChecked(true);
+                    }if (serv.isIntermediate()){
+                        cbintermediate.setChecked(true);
+                    }if (serv.isSUV()){
+                        cbSUV.setChecked(true);
+                    }if (serv.isPickupdate()){
+                        cbpickupdate.setChecked(true);
+                    }if (serv.isPickuptime()){
+                        cbpickuptime.setChecked(true);
+                    }if (serv.isReturndate()){
+                        cbreturndate.setChecked(true);
+                    }if (serv.isReturntime()){
+                        cbreturntime.setChecked(true);
+                    }if (serv.isMovingstartlocation()){
+                        cbmovingstartlocation.setChecked(true);
+                    }if (serv.isMovingendlocation()){
+                        cbmovingendlocation.setChecked(true);
+                    }if (serv.isArea()){
+                        cbarea.setChecked(true);
+                    }if (serv.isKmdriven()){
+                        cbkmdriven.setChecked(true);
+                    }if (serv.isNumberofmovers()){
+                        cbnumberofmovers.setChecked(true);
+                    }if (serv.isNumberofboxes()){
+                        cbnumberofboxes.setChecked(true);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
         final Button buttonUpdate = (Button) dialogView.findViewById(R.id.updateServiceButton);
         final Button buttonDelete = (Button) dialogView.findViewById(R.id.deleteServiceButton);
 
@@ -155,16 +250,14 @@ public class EditService extends AppCompatActivity {
                     editTextRate.requestFocus();
                     valid = false;
                 }
-                if (!editTextRate.getText().toString().trim().matches("^[0-9]*$")){
+                if (!editTextRate.getText().toString().trim().matches("[0-9]+(\\.){0,1}[0-9]*")){
                     editTextRate.setError("Rate must be a number!");
                     editTextRate.requestFocus();
                     valid = false;
                 }
 
                 if (valid){
-
-
-                    updateService( servID);
+                    updateService(servID);
                     b.dismiss();
                 }
             }
@@ -180,7 +273,6 @@ public class EditService extends AppCompatActivity {
     }
 
 
-
     public void deleteService(String servID){ //this does not delete service from authenticated database. but does delete from realtime.
         dbRef.child(servID).removeValue();
         Toast.makeText(getApplicationContext(), "Service deleted", Toast.LENGTH_LONG).show();
@@ -189,147 +281,101 @@ public class EditService extends AppCompatActivity {
 
     public void updateService(String servID){
 
-        if(cbfirstName.isChecked()){
-            firstName = true;
-        }else{
-            firstName = false;
-        }
-
-        if(cblastName.isChecked()){
-            lastName = true;
-        }else{
-            lastName = false;
-        }
-
-        if(cbdob.isChecked()){
-            dob = true;
-        }else{
-            dob = false;
-        }
-
-        if(cbaddress.isChecked()){
-            address = true;
-        }else{
-            address = false;
-        }
-
-        if(cbemail.isChecked()){
-            email = true;
-        }else{
-            email = false;
-        }
-
-        if(cbG1.isChecked()){
-            G1 = true;
-        }else{
-            G1 = false;
-        }
-
-        if(cbG2.isChecked()){
-            G2 = true;
-        }else{
-            G2 = false;
-        }
-
-        if(cbG3.isChecked()){
-            G3 = true;
-        }else{
-            G3 = false;
-        }
-
-        if(cbcompact.isChecked()){
-            compact = true;
-        }else{
-            compact = false;
-        }
-
-        if(cbintermediate.isChecked()){
-            intermediate = true;
-        }else{
-            intermediate = false;
-        }
-
-        if(cbSUV.isChecked()){
-            SUV = true;
-        }else{
-            SUV = false;
-        }
-
-        if(cbpickupdate.isChecked()){
-            pickupdate = true;
-        }else{
-            pickupdate = false;
-        }
-
-        if(cbpickuptime.isChecked()){
-            pickuptime = true;
-        }else{
-            pickuptime = false;
-        }
-
-        if(cbreturndate.isChecked()){
-            returndate = true;
-        }else{
-            returndate = false;
-        }
-
-        if(cbreturntime.isChecked()){
-            returntime = true;
-        }else{
-            returntime = false;
-        }
-
-        if(cbmovingstartlocation.isChecked()){
-            movingstartlocation = true;
-        }else{
-            movingstartlocation = false;
-        }
-
-        if(cbmovingendlocation.isChecked()){
-            movingendlocation = true;
-        }else{
-            movingendlocation = false;
-        }
-
-        if(cbarea.isChecked()){
-            area = true;
-        }else{
-            area = false;
-        }
-
-        if(cbkmdriven.isChecked()){
-            kmdriven = true;
-        }else{
-            kmdriven = false;
-        }
-
-        if(cbnumberofmovers.isChecked()){
-            numberofmovers = true;
-        }else{
-            numberofmovers = false;
-        }
-
-        if(cbnumberofboxes.isChecked()){
-            numberofboxes = true;
-        }else{
-            numberofboxes = false;
-        }
         if (cbisoffered.isChecked()){
-            isOffered = true;
+            dbRef.child(servID).child("offered").setValue(true);
         }else{
-            isOffered = false;
+            dbRef.child(servID).child("offered").setValue(false);
+        }if (cbfirstName.isChecked()){
+            dbRef.child(servID).child("firstName").setValue(true);
+        }else{
+            dbRef.child(servID).child("firstName").setValue(false);
+        }if (cblastName.isChecked()){
+            dbRef.child(servID).child("lastName").setValue(true);
+        }else{
+            dbRef.child(servID).child("lastName").setValue(false);
+        }if (cbdob.isChecked()){
+            dbRef.child(servID).child("dob").setValue(true);
+        }else{
+            dbRef.child(servID).child("dob").setValue(false);
+        }if (cbaddress.isChecked()){
+            dbRef.child(servID).child("address").setValue(true);
+        }else{
+            dbRef.child(servID).child("address").setValue(false);
+        }if (cbemail.isChecked()){
+            dbRef.child(servID).child("email").setValue(true);
+        }else{
+            dbRef.child(servID).child("email").setValue(false);
+        }if (cbG1.isChecked()){
+            dbRef.child(servID).child("g1").setValue(true);
+        }else{
+            dbRef.child(servID).child("g1").setValue(false);
+        }if (cbG2.isChecked()){
+            dbRef.child(servID).child("g2").setValue(true);
+        }else{
+            dbRef.child(servID).child("g2").setValue(false);
+        }if (cbG3.isChecked()){
+            dbRef.child(servID).child("g3").setValue(true);
+        }else{
+            dbRef.child(servID).child("g3").setValue(false);
+        }if (cbcompact.isChecked()){
+            dbRef.child(servID).child("compact").setValue(true);
+        }else{
+            dbRef.child(servID).child("compact").setValue(false);
+        }if (cbintermediate.isChecked()){
+            dbRef.child(servID).child("intermediate").setValue(true);
+        }else{
+            dbRef.child(servID).child("intermediate").setValue(false);
+        }if (cbSUV.isChecked()){
+            dbRef.child(servID).child("suv").setValue(true);
+        }else{
+            dbRef.child(servID).child("suv").setValue(false);
+        }if (cbpickupdate.isChecked()){
+            dbRef.child(servID).child("pickupdate").setValue(true);
+        }else{
+            dbRef.child(servID).child("pickupdate").setValue(false);
+        }if (cbpickuptime.isChecked()){
+            dbRef.child(servID).child("pickuptime").setValue(true);
+        }else{
+            dbRef.child(servID).child("pickuptime").setValue(false);
+        }if (cbreturndate.isChecked()){
+            dbRef.child(servID).child("returndate").setValue(true);
+        }else{
+            dbRef.child(servID).child("returndate").setValue(false);
+        }if (cbreturntime.isChecked()){
+            dbRef.child(servID).child("returntime").setValue(true);
+        }else{
+            dbRef.child(servID).child("returntime").setValue(false);
+        }if (cbmovingstartlocation.isChecked()){
+            dbRef.child(servID).child("movingstartlocation").setValue(true);
+        }else{
+            dbRef.child(servID).child("movingstartlocation").setValue(false);
+        }if (cbmovingendlocation.isChecked()){
+            dbRef.child(servID).child("movingendlocation").setValue(true);
+        }else{
+            dbRef.child(servID).child("movingendlocation").setValue(false);
+        }if (cbarea.isChecked()){
+            dbRef.child(servID).child("area").setValue(true);
+        }else{
+            dbRef.child(servID).child("area").setValue(false);
+        }if (cbkmdriven.isChecked()){
+            dbRef.child(servID).child("kmdriven").setValue(true);
+        }else{
+            dbRef.child(servID).child("kmdriven").setValue(false);
+        }if (cbnumberofmovers.isChecked()){
+            dbRef.child(servID).child("numberofmovers").setValue(true);
+        }else{
+            dbRef.child(servID).child("numberofmovers").setValue(false);
+        }if (cbnumberofboxes.isChecked()){
+            dbRef.child(servID).child("numberofboxes").setValue(true);
+        }else {
+            dbRef.child(servID).child("numberofboxes").setValue(false);
         }
 
-        rate = Double.parseDouble(editTextRate.getText().toString().trim());
+        newRate = Double.parseDouble(editTextRate.getText().toString().trim());
         newName = editTextName.getText().toString().trim();
 
-        NewService serv = new NewService(address, area,compact,dob, email, firstName,
-                G1, G2, G3, intermediate, kmdriven, lastName, movingendlocation,
-                movingstartlocation, newName,  numberofboxes,numberofmovers,
-                pickupdate, pickuptime,rate,returndate, returntime,
-                SUV,isOffered, servID);
-
-        dbRef.child(servID).setValue(serv);
+        dbRef.child(servID).child("rate").setValue(newRate);
+        dbRef.child(servID).child("name").setValue(newName);
         Toast.makeText(getApplicationContext(), "Service Updated", Toast.LENGTH_LONG).show();
     }
 
