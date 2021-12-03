e670cd6b797f3371472cc5e73fbb6fb6406cddf0
yasser <yatta027@uottawa.ca>
2021-12-03 14:53:10 -0500

    yasser

--- projects/app/src/main/java/com/example/segproject/EmployeeActivity.java
+++ projects/app/src/main/java/com/example/segproject/EmployeeActivity.java
@@ -112,15 +112,15 @@ public class EmployeeActivity extends AppCompatActivity {
                     valid = false;
                 }
 //                if (eTZip.getText().toString().replaceAll("//s", "").length() == 6){ // canadian zip
-<<<<<<< HEAD
+
 //                    String temp = eTZip.getText().toString().replaceAll("//s", "");
 //                    if (!( Character.isLetter(temp.charAt(0)) && Character.isDigit(temp.charAt(1)) && Character.isLetter(temp.charAt(2)) && Character.isDigit(temp.charAt(3)) && Character.isLetter(temp.charAt(4)) && Character.isDigit(temp.charAt(5)) ))
 //                        eTZip.setError("Please enter a valid postal code!");
 //                        eTZip.requestFocus();
-=======
+
 //                    String temp = eTZip.getText().toString();
 //                    if (!( Character.isLetter(temp.charAt(0)) && Character.isDigit(temp.charAt(1)) && Character.isLetter(temp.charAt(2)) && Character.isDigit(temp.charAt(3)) && Character.isLetter(temp.charAt(4)) && Character.isDigit(temp.charAt(0)) ))
->>>>>>> 4100343390b716fcb428ccd69550d5a4173e3d55
+
 //                        valid = false;
 //                }
 
